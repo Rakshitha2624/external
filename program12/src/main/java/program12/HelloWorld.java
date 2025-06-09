d506a72a1e13d7e14d24f19a6bf76a3b7e6894c4
ksit <ksit@madlab13>
2025-06-09 13:34:19 +0530

    initial commit

---
+++ program12/src/main/java/program12/HelloWorld.java
@@ -0,0 +1,12 @@
+package program12;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=4;
+		int b=4;
+		System.out.println(a+b);
+	}
+
+}
